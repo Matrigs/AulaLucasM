f80b3b33645473d209e9c4a5c26d5b0a1328cbde
Lucas MDS <cristianevfms@gmail.com>
2021-07-01 20:10:57 -0300

    PlayerComponent

---
+++ Assets/scripts/PlayerComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour
+{
+    public float moveDeitado;
+
+    public float velocidade = 20;
+
+    public bool jumpar = false;
+}
